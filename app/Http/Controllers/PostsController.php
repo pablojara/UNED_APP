df5c35a9715e175d6829a07a59187582fc85b4f8
Pablo Jara <jaramillodominguezpablo@gmail.com>
2019-09-03 20:48:46 +0200

    fix redirect to dashboard

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -129,7 +129,7 @@ class PostsController extends Controller
         $post->cover_image = $fileNameToStore;
         $post->save();
 
-        return redirect('/dashboard')->with('success', 'Post Created');
+        return redirect('/dashboard')->with('success', 'Contenido creado');
     }
 
     /**
@@ -205,7 +205,7 @@ class PostsController extends Controller
         }
         $post->save();
 
-        return redirect('/posts')->with('success', 'Post Updated');
+        return redirect('/dashboard')->with('success', 'Contenido actualizado');
     }
 
     /**
